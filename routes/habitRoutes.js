92512647fa14701a783052207008ac4042a0d367
conqueror24 <144806114+conqueror24@users.noreply.github.com>
2024-12-04 10:26:14 +0530

    Update habitRoutes.js

--- routes/habitRoutes.js
+++ routes/habitRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const habitController = require('../controllers/habitController');
 
-// Define routes
 router.post('/habits', habitController.addHabit);
 router.put('/habits/:id', habitController.updateHabit);
 router.get('/habits', habitController.getHabits);
